e6d214d7bc72e2bf62dabc2c347f48198f75f99f
Sarah Ford <sford85@gmail.com>
2016-08-26 14:12:14 -0700

    view functions are working, ready for integration with visualization

--- src/app.js
+++ src/app.js
@@ -6,7 +6,6 @@ import '../scss/main.scss';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import io from 'socket.io-client';
-import ajax from 'superagent';
 import { ipcRenderer } from 'electron';
 
 const { Router, Route, Link, hashHistory, IndexRoute } = require('react-router');
@@ -15,25 +14,21 @@ import Login from './login';
 import GitTree from './gitTree';
 import Terminal from './terminal/terminal.js'
 
-// Socket handling for app. Must be global to current page for ipcRenderer + React
-let socket = io('http://navigitorsite.herokuapp.com');
-let socketRoom = null;
-
-/* listens for an git commit event from main.js webContent.send
- then sends commit string to the server via socket */
-ipcRenderer.on('commitMade', function(event, arg){
-	if(socketRoom) socket.emit('broadcastCommit', {'room': socketRoom, 'data': JSON.stringify(arg, null, 4)});
-});
-
-/* listens for an git branch checkout event from main.js webContent.send
- then sends commit string to the server via socket */
-ipcRenderer.on('changedBranches', function(event, arg){
-	if(socketRoom) socket.emit('broadcastBranch', {'room': socketRoom, 'data': JSON.stringify(arg, null, 4)});
-});
-
-function dirChoice() {
-	ipcRenderer.send('dirChoice');
-}
+// // Socket handling for app. Must be global to current page for ipcRenderer + React
+// let socket = io('http://navigitorsite.herokuapp.com');
+// let socketRoom = null;
+//
+// /* listens for an git commit event from main.js webContent.send
+//  then sends commit string to the server via socket */
+// ipcRenderer.on('commitMade', function(event, arg){
+// 	if(socketRoom) socket.emit('broadcastCommit', {'room': socketRoom, 'data': JSON.stringify(arg, null, 4)});
+// });
+//
+// /* listens for an git branch checkout event from main.js webContent.send
+//  then sends commit string to the server via socket */
+// ipcRenderer.on('changedBranches', function(event, arg){
+// 	if(socketRoom) socket.emit('broadcastBranch', {'room': socketRoom, 'data': JSON.stringify(arg, null, 4)});
+// });
 
 
 class App extends Component {
@@ -56,40 +51,10 @@ class App extends Component {
 		this.setState({ message: this.state.message.concat(data) });
 	}
 
-	_handleSubmit(e) {
-		e.preventDefault();
+	// function dirChoice() {
+	// 	ipcRenderer.send('dirChoice');
+	// }
 
-		let orgName = document.getElementById('login-org').value;
-		let repoName = document.getElementById('login-repo').value;
-
-		ajax.get(`https://api.github.com/repos/${orgName}/${repoName}/commits`)
-			.end((error, response) => {
-				if (!error && response) {
-					let apiData = response.body.map(function(item){
-						return {
-							name: item.commit.author.name,
-							date: item.commit.author.date,
-							message: item.commit.message
-						}
-					}).reverse();
-					this.setState({ message: apiData });
-					console.log(apiData);
-				} else {
-					console.log('error fetching Github data', error);
-				}
-				if(socketRoom) socket.emit("unsubscribe", { room: socketRoom });
-				socket.emit("subscribe", { room: `${orgName}/${repoName}live` });
-				socketRoom = `${orgName}/${repoName}live`;
-			}
-		);
-
-		// Save for now to transfer to main process later
-		// let githubLogin = {
-		// 	orgName: orgName,
-		// 	repoName: repoName
-		// }
-		// ipcRenderer.send('githubLogin', githubLogin);
-	}
 
 	render() {
     return (
