03d0bf86eb4c7d5eb61b0ae3593a817d739adbc6
Colin Brownlie <colinmbrownlie@gmail.com>
2016-09-07 15:56:34 -0700

    analytics working w/dummy data

--- src/analytics.js
+++ src/analytics.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
-import $ from 'jquery'
+import $ from 'jquery';
+import {BarChart} from 'react-easy-chart'
+//import rd3 from 'react-d3-library';
+//const BarChart = rd3.BarChart
 
 export default class Analytics extends Component {
 
@@ -24,24 +27,44 @@ export default class Analytics extends Component {
           else users[name] = 1
         })
         console.log('users: ', users)
-        let chart = {data: []};
-        for (let key in users) {
-          console.log('key: ', key)
-          chart.data.push({x: key, y: users[key]})
-        }
-        console.log('chart: ', chart)
-      }
+        let chart = [];
+        // for (let key in users) {
+        //   console.log('key: ', key)
+        //   chart.push({x: key, y: users[key]})
+        // }
+        chart.push({x: "Colin", y: 12}, {x: "Sarah", y: 6}, {x: "Steve", y: 9}, {x: "Binh", y: 14})
+        let jason = JSON.stringify(chart)
+        console.log('chart: ', jason)
+        this.props.setAppState({commitsPerUser: chart})
+      }.bind(this)
     })
   }
+
   render() {
-    console.log('storage:', sessionStorage)
+    let bar = null;
+      if (this.props.getAppState.commitsPerUser) {
+        const style = {textAlign: 'center'}
+      bar = <BarChart
+      data={this.props.getAppState.commitsPerUser}
+      colorBars
+      height={450}
+      width={500}
+      margin={{top: 60, right: 30, bottom: 50, left: 70}}
+      axes
+      //barWidth={100}
+      axisLabels={{y: 'Commits'}}
+      yTickNumber={5}
+      />
+  }
     return (
         <div id='analytics-container'>
             <h5>Get commits for the following number of days:</h5>
             <form id='numDays'>
               <input id='daysInput' type='text' />
-              <button id="submitDays" onClick={this.getCollection}>submit days</button>
+              <button id="submitDays" onClick={this.getCollection.bind(this)}>submit days</button>
+              
             </form>
+            {bar}
         </div>
     )
   }
