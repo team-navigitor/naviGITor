9d6b1f97f53cafbbdad8e05b86792c4132a34e1d
Colin Brownlie <colinmbrownlie@gmail.com>
2016-09-16 14:55:45 -0700

    comments added for team analytics

--- src/analytics.js
+++ src/analytics.js
@@ -2,32 +2,47 @@ import React, {Component} from 'react';
 import $ from 'jquery'
 import {BarChart} from 'react-easy-chart';
 
-//adding comments
+
 export default class TeamAnalytics extends Component {
 
-  getCollection(e) {
+/* 
+  @param: (event) - click event
+*/
+  getEventsByTime(e) {
     
+    //prevent reload on submit
     e.preventDefault();
-    let time = Math.floor((Date.now() - ($('#daysInput').val() * 86400000)) / 1000);
-    let chart = [];
-    let users = {};
+    /*because Git stores time in seconds vs milliseconds, 
+    to get events going back the number of days chosen by the user,
+    we multiply that by milliseconds in a day, then divide by 1000
+    */
+    let time = Math.floor((Date.now() - ($('#daysInput').val() * 86400000)) / 1000),
+    chart = [],
+    users = {};
+
+    //get team & repo name from state, then filter by time
     this.props.getAppState.teamData.filter(el => {
       return el.time > time;
     }).forEach(elem => {
-      console.log('new elem ' + elem.user)
+      //parse name
       let name = elem.user.substring(0, elem.user.indexOf('<') - 1)
+      //if name is in user object, increment. Otherwise add name to user object
       if (users[name]) users[name]++;
       else users[name] = 1;
     })
+    //push objects into chart in format needed by react easy chart
     for (let key in users) {
       chart.push({x: key, y: users[key]})
     }
+    //add chart array to state
     this.props.setAppState({commitsPerUser: chart})
   }
 
   render() {
+    //initialize bar
     let bar = null;
 
+    //if chart array is in state, create bar chart
       if (this.props.getAppState.commitsPerUser) {
         const style = {textAlign: 'center'}
 
@@ -42,6 +57,7 @@ export default class TeamAnalytics extends Component {
       axisLabels={{y: 'Commits'}}
       yTickNumber={5}
       />
+  // if chart array isn't in state, display logo
   } else {
     bar = <img height='250px' width='250px' src="./images/darknaviGitorLogo_1.png" />
   }
