c4ab6274c62429c9a0dc545915c1a046d90a4f77
binh.x.nguyen <binhxn89@gmail.com>
2016-09-16 12:05:53 -0700

    Added standardized comments for noddeModal and teamGitTree

--- src/gitTree/nodeModal.js
+++ src/gitTree/nodeModal.js
@@ -1,25 +1,29 @@
-// import { ipcRenderer } from 'electron';
-var ipcRenderer = require('electron').ipcRenderer;
-var $ = require('jquery');
-// import $ from 'jquery';
+import { ipcRenderer } from 'electron';
+import $ from 'jquery';
 
 
 $(document).ready(function() {
 	ipcRenderer.send('nodeModalWindowReady');
 
+	/**
+	 * Listens for node click event - 'newGlobalGitNode' - to open modal with statistics
+	 * @param {String} - event
+	 * @param {Object} - data
+	 * @return {String} appends to HTML elements in modal
+	 */
 	ipcRenderer.on('nodeModalWindow', function(event, data){
 		$('.node-commit').append(data.commit);
 		$('.node-author').append(data.author);
 		$('.node-event').append(data.event);
 		$('.node-date').append(data.date);
 		$('.node-time').append(data.time);
-		if (data.diff !== undefined) $('.node-added').append(data.diffStats.adds)
-		if (data.diff !== undefined) $('.node-deleted').append(data.diffStats.subs)
-		console.log('hello from modalFrminside' + JSON.stringify(data));
+		if (data.diff !== undefined) $('.node-added').append(data.diffStats.adds);
+		if (data.diff !== undefined) $('.node-deleted').append(data.diffStats.subs);
 	})
 
+	// Close Modal button
 	document.getElementById("exit-button").addEventListener("click", function (e) {
-		 ipcRenderer.send('closeModal');
+		ipcRenderer.send('closeModal');
 	});
 
 });
