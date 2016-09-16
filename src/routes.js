c9e43b76cf2d7f6a94f9a340f58f902836f8d9bd
Sarah Ford <sford85@gmail.com>
2016-09-16 16:11:02 -0700

    cleaned up routes page

--- src/routes.js
+++ src/routes.js
@@ -1,51 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-const { Router, Route, Link, hashHistory, IndexRoute } = require('react-router');
+
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+// const { Router, Route, hashHistory, IndexRoute } = require('react-router');
 
 import App from './app';
-
-import Main from './Pages/main';
 import Login from './Pages/login';
 import Signup from './Pages/signup';
 import TeamLogin from './Pages/teamLogin';
-
+import Main from './Pages/main';
+import Logo from './logopage';
 import TeamGitTree from './gitTree/teamGitTree';
 import LocalGitTree from './gitTree/localGitTree';
 import TerminalView from './terminal/terminal.js';
 import TeamAnalytics from './analytics.js';
 import LocalGraph from './localGraph';
-import Logo from './logopage';
 import Profile from './profilePage';
 
+function Routes() {
+  return (
+    <Router history={hashHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={Login} />
+        <Route path="Signup" component={Signup} />
+        <Route path="TeamLogin" component={TeamLogin} />
 
-
-export default class Routes extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <Router history = {hashHistory}>
-         <Route path = "/" component = {App}>
-           <IndexRoute component = {Login} />
-           <Route path = "Signup" component = {Signup} />
-           <Route path = "TeamLogin" component = {TeamLogin} />
-
-           <Route path = "Main" component = {Main}>
-             <IndexRoute component = {Logo} />
-             <Route path = "TeamGitTree" component = {TeamGitTree} />
-             <Route path = "LocalGitTree" component = {LocalGitTree} />
-             <Route path = "TeamAnalytics" component = {TeamAnalytics} />
-             <Route path = "Terminal" component = {TerminalView} />
-             <Route path = "LocalGraph" component = {LocalGraph} />
-             <Route path = "Profile" component = {Profile} />
-
-           </Route>
-         </Route>
-      </Router>
-    );
-  }
+        <Route path="Main" component={Main}>
+          <IndexRoute component={Logo} />
+          <Route path="TeamGitTree" component={TeamGitTree} />
+          <Route path="LocalGitTree" component={LocalGitTree} />
+          <Route path="TeamAnalytics" component={TeamAnalytics} />
+          <Route path="Terminal" component={TerminalView} />
+          <Route path="LocalGraph" component={LocalGraph} />
+          <Route path="Profile" component={Profile} />
+        </Route>
+      </Route>
+    </Router>
+  );
 }
 
 ReactDOM.render((<Routes />), document.getElementById('app'));
