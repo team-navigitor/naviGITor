2dfb3cf0f287a4ce55c32dc9aa98ab7a1396956b
Sarah Ford <sford85@gmail.com>
2016-09-16 16:34:50 -0700

    cleaned up login page

--- src/Pages/login.js
+++ src/Pages/login.js
@@ -1,12 +1,10 @@
-import React, {Component} from 'react';
-import { Router, Route, Link } from 'react-router';
-import $ from 'jquery'
-import { hashHistory } from 'react-router';
+import React, { Component } from 'react';
+import { Link, hashHistory } from 'react-router';
+import $ from 'jquery';
+import { ipcRenderer } from 'electron';
+
 import Signup from './signup';
 import TeamLogin from './teamLogin';
-import { ipcRenderer } from 'electron';
-const gitParser = require('../gitParser/gitparser.js');
-
 
 
 export default class Login extends Component {
@@ -15,50 +13,50 @@ export default class Login extends Component {
     this.submitPassword = this.submitPassword.bind(this);
   }
 
-  verifyUser(){
-    let user = {
+  verifyUser() {
+    const user = {
       name: $('#userNameInput').val(),
-      password: $('#userPasswordInput').val()
-    }
+      password: $('#userPasswordInput').val(),
+    };
 
     $.ajax({
       method: 'POST',
       url: 'http://navigitorsite.herokuapp.com/verify',
       data: user,
-      success: function(data) {
+      success: function (data) {
         if (data.github) {
           ipcRenderer.send('avatarUrl', data.github);
-          this.props.setAppState({username: user.name});
-          this.props.setAppState({githubAvatar: 'https://avatars.githubusercontent.com/'+data.github});
+          this.props.setAppState({ username: user.name });
+          this.props.setAppState({ githubAvatar: `https://avatars.githubusercontent.com/${data.github}` });
           hashHistory.push('/TeamLogin');
-        }
-        else if (data) {
-          console.log('no github ');
-          hashHistory.push('/TeamLogin');
-        }
+        } else if (data) hashHistory.push('/TeamLogin');
         else hashHistory.push('/Signup');
-      }.bind(this)
+      }.bind(this),
     });
   }
 
-  submitPassword(e){
+  submitPassword(e) {
     e.preventDefault();
     this.verifyUser();
   }
 
   render() {
     return (
-      <div className='login-container'>
-        <img className='login-logo' src='./images/darknaviGitorLogo_1.png' />
-        <form className='login-form'>
-            <input id='userNameInput' placeholder='USERNAME' type='text' required />
-            <input id='userPasswordInput' type='password' placeholder='PASSWORD' required />
-            <button className='login-button' type="submit" onClick={this.submitPassword}>LOG IN</button>
+      <div className="login-container">
+        <img className="login-logo" src="./images/darknaviGitorLogo_1.png" role="presentation" />
+        <form className="login-form">
+          <input id="userNameInput" placeholder="USERNAME" type="text" required />
+          <input id="userPasswordInput" type="password" placeholder="PASSWORD" required />
+          <button className="login-button" type="submit" onClick={this.submitPassword}>LOG IN</button>
         </form>
-        <div id='signup-button'>
-          <p>Don't have an account? <Link to='Signup' className='signup-link'>Sign Up</Link></p>
+        <div id="signup-button">
+          <p>Don't have an account? <Link to="Signup" className="signup-link">Sign Up</Link></p>
         </div>
-    </div>
-    )
+      </div>
+    );
   }
 }
+
+Login.propTypes = {
+  setAppState: React.PropTypes.func,
+};
